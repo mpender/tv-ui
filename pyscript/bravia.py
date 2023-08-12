36c7c0550fd0071c500197d357928bd71720ca1a
Mark Pender <markpender180@gmail.com>
2023-08-12 22:26:16 +0930

    added sourcecode base

---
+++ pyscript/bravia.py
@@ -0,0 +1,105 @@
+import datetime
+import requests
+import json
+import os
+import configparser
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    URL = f"http://{os.getenv('URL')}/sony/"
+    # password = os.getenv('PASSWORD')
+    headers = {"X-Auth-PSK": os.getenv("PASSWORD"), "Content-Type": "application/xml"}
+    # print(headers)
+    # headers = json.dumps(headers)
+    # with open('./test') as f:
+    #    lines = f.readlines()
+    #    print(lines)
+    #    close()
+except:
+    print(f'Opps, couldnt find environment vars, please validate via "printenv"')
+    raise
+
+
+def give_date():
+    current_time = datetime.datetime.now()
+    return current_time
+
+
+def tv_power_on():
+    payload = '{\n    "method": "setPowerStatus",\n    "id": 55,\n    "params": [{"status": true\n    }],\n    "version": "1.0"\n}'
+    response = requests.request("POST", URL + "system", headers=headers, data=payload)
+    return response.text
+
+
+def tv_power_off():
+    payload = '{\n    "method": "setPowerStatus",\n    "id": 55,\n    "params": [{"status": false\n    }],\n    "version": "1.0"\n}'
+    response = requests.request("POST", URL + "system", headers=headers, data=payload)
+    return response.text
+
+
+def tv_volume_up():
+    payload = json.dumps(
+        {
+            "method": "setAudioVolume",
+            "id": 98,
+            "params": [{"volume": "+1", "ui": "on", "target": "speaker"}],
+            "version": "1.2",
+        }
+    )
+    response = requests.request("POST", URL + "audio", headers=headers, data=payload)
+    return response.text
+
+
+def tv_volume_down():
+    payload = {
+        "method": "setAudioVolume",
+        "id": 98,
+        "params": [{"volume": "-1", "ui": "on", "target": "speaker"}],
+        "version": "1.2",
+    }
+    response = requests.request(
+        "POST", URL + "audio", headers=headers, data=json.dumps(payload)
+    )
+    return response.text
+
+
+def tv_volume_quiet():
+    payload = {
+        "method": "setAudioVolume",
+        "id": 98,
+        "params": [{"volume": "4", "ui": "on", "target": "speaker"}],
+        "version": "1.2",
+    }
+    response = requests.request(
+        "POST", URL + "audio", headers=headers, data=json.dumps(payload)
+    )
+    return response.text
+
+
+def switch_to_hdmi():
+    payload = {
+        "method": "setPlayContent",
+        "id": 101,
+        "params": [{"uri": "extInput:hdmi?port=4"}],
+        "version": "1.0",
+    }
+    response = requests.request(
+        "POST", URL + "avContent", headers=headers, data=json.dumps(payload)
+    )
+    return response.text
+
+
+def enter():
+    payload = '<s:Envelope\n    xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"\n    s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">\n    <s:Body>\n        <u:X_SendIRCC xmlns:u="urn:schemas-sony-com:service:IRCC:1">\n            <IRCCCode>AAAAAQAAAAEAAABlAw==</IRCCCode>\n        </u:X_SendIRCC>\n    </s:Body>\n</s:Envelope>'
+    headers = {
+        "SOAPACTION": '"urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"',
+        "X-Auth-PSK": "1234",
+        "Content-Type": "text/xml; charset=UTF-8",
+    }
+
+    response = requests.request("POST", URL + "ircc", headers=headers, data=payload)
+
+    return response.text
---
+++ pyscript/start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+python3 -m http.server 8081 --bind 0.0.0.0
